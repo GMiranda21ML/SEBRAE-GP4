d61d07b6a83521448c1fb5e57dfeead1ce5e5712
AUGUSTO MALHEIROS <gutopunkmalheiros@gmail.com>
2025-10-16 11:34:44 -0300

    Implementado a classe de consumo de API

---
+++ src/main/java/br/com/sebrae/projetos/grupo04/consumoapi/ConsumoAPI.java
@@ -0,0 +1,31 @@
+package br.com.sebrae.projetos.grupo04.consumoapi;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoAPI {
+
+    public String ObterDados(String endereco){
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().
+                uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = null;
+
+        try{
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        }catch (IOException e){
+            throw new RuntimeException(e); //usar serr
+        }catch (InterruptedException e ){
+            throw new RuntimeException(e); //usar serr
+        }
+
+        String json = response.body();
+        return json;
+    }
+}
