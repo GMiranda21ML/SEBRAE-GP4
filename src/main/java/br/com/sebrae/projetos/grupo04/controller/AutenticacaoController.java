f4ec43f91b1fb223933846247632abe00e769889
Rodrigo Dantas <rodrigo6031@gmail.com>
2025-10-28 12:02:03 -0300

    Corrigindo service e controller de usuario e autenticacao

--- src/main/java/br/com/sebrae/projetos/grupo04/controller/AutenticacaoController.java
+++ src/main/java/br/com/sebrae/projetos/grupo04/controller/AutenticacaoController.java
@@ -4,14 +4,10 @@ import br.com.sebrae.projetos.grupo04.DTO.RespostaLoginDTO;
 import br.com.sebrae.projetos.grupo04.DTO.UsuarioCadastroDTO;
 import br.com.sebrae.projetos.grupo04.DTO.UsuarioLoginDTO;
 import br.com.sebrae.projetos.grupo04.model.Usuario;
-import br.com.sebrae.projetos.grupo04.repository.UsuarioRepository;
-import br.com.sebrae.projetos.grupo04.security.TokenService;
+import br.com.sebrae.projetos.grupo04.service.UsuarioService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,29 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutenticacaoController {
 
     @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
-    private UsuarioRepository repository;
-    @Autowired
-    private TokenService tokenService;
+    private UsuarioService usuarioService;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody @Valid UsuarioLoginDTO dto) {
-        var usuarioSenha = new UsernamePasswordAuthenticationToken(dto.email(),dto.senha());
-        var auth = this.authenticationManager.authenticate(usuarioSenha);
-        var token = tokenService.generateToken((Usuario) auth.getPrincipal());
+        String token = usuarioService.login(dto);
         return ResponseEntity.ok().body(new RespostaLoginDTO(token));
     }
 
     @PostMapping("/cadastro")
     public ResponseEntity<Usuario> cadastro(@RequestBody @Valid UsuarioCadastroDTO dto) {
-        if (repository.findByEmail(dto.email()) != null) {
-            return ResponseEntity.badRequest().build();
-        }
-        String encryptedPassword = new BCryptPasswordEncoder().encode(dto.senha());
-        Usuario usuario = new Usuario(dto.nome(),dto.email(),encryptedPassword,dto.role());
-        repository.save(usuario);
-        return ResponseEntity.ok().build();
+        Usuario novoUsuario = usuarioService.cadastro(dto);
+        return ResponseEntity.ok(novoUsuario);
     }
-
 }
--- src/main/java/br/com/sebrae/projetos/grupo04/service/UsuarioService.java
+++ src/main/java/br/com/sebrae/projetos/grupo04/service/UsuarioService.java
@@ -4,13 +4,12 @@ import br.com.sebrae.projetos.grupo04.DTO.UsuarioCadastroDTO;
 import br.com.sebrae.projetos.grupo04.DTO.UsuarioLoginDTO;
 import br.com.sebrae.projetos.grupo04.model.Usuario;
 import br.com.sebrae.projetos.grupo04.repository.UsuarioRepository;
+import br.com.sebrae.projetos.grupo04.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -24,26 +23,22 @@ public class UsuarioService {
     private PasswordEncoder passwordEncoder;
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    private TokenService tokenService;
 
-//    public Usuario login(UsuarioLoginDTO dto) {
-//        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(dto.email(), dto.senha());
-//        Authentication autenticacao = authenticationManager.authenticate(authenticationToken);
-//
-//        SecurityContext context = SecurityContextHolder.createEmptyContext();
-//        context.setAuthentication(autenticacao);
-//        SecurityContextHolder.setContext(context);
-//
-//        return (Usuario) autenticacao.getPrincipal();
-//    }
-//
-//    public Usuario cadastro(UsuarioCadastroDTO dto) {
-//        if (repository.existsByEmail(dto.email())) {
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Já existe um usuario com este email");
-//        }
-//
-//        Usuario usuario = new Usuario(dto.nome(),dto.email(), dto.senha(), dto.role());
-//        repository.save(usuario);
-//        return usuario;
-//    }
+    public String login(UsuarioLoginDTO dto) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(dto.email(), dto.senha());
+        Authentication autenticacao = authenticationManager.authenticate(authenticationToken);
+        return tokenService.generateToken((Usuario) autenticacao.getPrincipal());
+    }
 
+    public Usuario cadastro(UsuarioCadastroDTO dto) {
+        if (repository.existsByEmail(dto.email())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Já existe um usuario com este email");
+        }
+
+        String encryptedPassword = passwordEncoder.encode(dto.senha());
+        Usuario usuario = new Usuario(dto.nome(), dto.email(), encryptedPassword, dto.role());
+        return repository.save(usuario);
+    }
 }
