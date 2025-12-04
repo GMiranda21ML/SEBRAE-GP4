ff8123c5b16ad34dbd48dc5980ca9bd1522fb9af
Rodrigo Dantas <rodrigo6031@gmail.com>
2025-12-04 13:45:32 -0300

    ajustando ordem das sugestoes

--- src/main/java/br/com/sebrae/projetos/grupo04/repository/SugestaoRepository.java
+++ src/main/java/br/com/sebrae/projetos/grupo04/repository/SugestaoRepository.java
@@ -2,9 +2,12 @@ package br.com.sebrae.projetos.grupo04.repository;
 
 import br.com.sebrae.projetos.grupo04.model.Sugestao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 import java.util.UUID;
 
 public interface SugestaoRepository extends JpaRepository<Sugestao, UUID> {
+    @Query("SELECT s FROM Sugestao s ORDER BY SIZE(s.curtidas) DESC, s.dataCriacao DESC")
     List<Sugestao> findAllByOrderByDataCriacaoDesc();
 }
